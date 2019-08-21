2c90eb099e30b6fb03175587caf494ec9df7f682
Kyle Purdon <kylepurdon@gmail.com>
2019-08-21 15:14:09 -0600

    Load configuration from environment not flags

--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-func main() {
-	port := flag.String("port", "8000", "port to listen on")
-	target := flag.String("target", "http://localhost:8001", "target host")
-	audience := flag.String("jwt-audience", "/projects/PROJECT_NUMBER/global/backendServices/SERVICE_ID", "the signed header jwt audience from cloud iap")
-	flag.Parse()
+func getEnv(name string, defaultValue string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		if defaultValue == "" {
+			log.Fatalf("environment variable %q is required", name)
+		}
+		return defaultValue
+	}
+	return value
+}
 
-	targetURL, err := url.Parse(*target)
+func main() {
+	port := getEnv("IAPAP_PORT", "8000")
+	target := getEnv("IAPAP_TARGET", "http://localhost:8001")
+	audience := getEnv("IAPAP_AUDIENCE", "")
+
+	targetURL, err := url.Parse(target)
 	if err != nil {
-		log.Fatal("target %q is an invalid url: %s", *target, err)
+		log.Fatal("target %q is an invalid url: %s", target, err)
 	}
 
 	r := chi.NewRouter()
@@ -33,9 +43,9 @@ func main() {
 		// TODO: check if target is available
 		w.WriteHeader(http.StatusOK)
 	})
-	r.Handle("/", auth(httputil.NewSingleHostReverseProxy(targetURL), *audience))
+	r.Handle("/", auth(httputil.NewSingleHostReverseProxy(targetURL), audience))
 
-	addr := net.JoinHostPort("", *port)
+	addr := net.JoinHostPort("", port)
 	log.Printf("iapap listening on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, r))
 }
